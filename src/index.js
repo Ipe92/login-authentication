bb32f512e2a782d5fde92ff2e7fe9a39036d7d58
Iippo Leppänen <21137181@edu.riveria.fi>
2021-06-10 12:20:29 +0300

    UPDATE readme

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
 
-//https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
-
 ReactDOM.render(
     <React.StrictMode>
         <App />
