19abcf873f954093c85724751bb6dab82db55965
Iippo Leppänen <21137181@edu.riveria.fi>
2021-06-21 11:01:43 +0300

    Update Login.js +proptypes, +usestate

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
 import "./Login.css";
 
-const Login = () => {
+const Login = ({ setToken }) => {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
     return (
         <div className="login-wrapper">
             <h1>Please Log In</h1>
             <form>
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input type="text" onChange={(event) => setUserName(event.target.value)} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" onChange={(event) => setPassword(event.target.value)} />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
@@ -21,4 +25,9 @@ const Login = () => {
         </div>
     );
 };
+
+Login.PropTypes = {
+    setToken: PropTypes.func.isRequired,
+};
+
 export default Login;
