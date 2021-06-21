216dd4af072412b3edc0e9ba4719a32ffbc83094
Iippo Leppänen <21137181@edu.riveria.fi>
2021-06-21 12:06:05 +0300

    Update Login.js now handles form onSubmit

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -16,17 +16,27 @@ async function loginUser(credentials) {
 const Login = ({ setToken }) => {
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password,
+        });
+        setToken(token);
+    };
+
     return (
         <div className="login-wrapper">
             <h1>Please Log In</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text" onChange={(event) => setUserName(event.target.value)} />
+                    <input type="text" onChange={(e) => setUserName(e.target.value)} />
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" onChange={(event) => setPassword(event.target.value)} />
+                    <input type="password" onChange={(e) => setPassword(e.target.value)} />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
